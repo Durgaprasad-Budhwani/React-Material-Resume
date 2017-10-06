492fa8e1c5acb439d68e158d738b331e01413a04
Durgaprasad Budhwani <durgaprasad.budhwani@gmail.com>
2017-10-06 21:21:12 +0530

    Change case of react

--- __mocks__/setup.mock.js
+++ __mocks__/setup.mock.js
@@ -1,14 +1,13 @@
 /**
  * Created by Durgaprasad Budhwani on 10/6/2017.
  */
+import React from 'react';
+
 jest.mock('react-animate-on-scroll', () => {
-    // const RealComponent = require.requireActual('react-animate-on-scroll');
-    const React = require('React');
     class ScrollAnimation extends React.Component {
         render() {
             return (<div>{this.props.children}</div>);
         }
     }
-    // ScrollAnimation.default = RealComponent.default;
     return ScrollAnimation;
 });
