6bbd1927f0690ffef9f95fdaa480cdd9f3667ee5
Durgaprasad <durgaprasad.budhwani@gmail.com>
2017-10-09 09:21:51 +0530

    removed local variable logic from mocks

--- __mocks__/setup.mock.js
+++ __mocks__/setup.mock.js
@@ -1,9 +1,8 @@
 /**
  * Created by Durgaprasad Budhwani on 10/6/2017.
  */
-import React from 'react';
-
 jest.mock('react-animate-on-scroll', () => {
+  const React = require('react');
     class ScrollAnimation extends React.Component {
         render() {
             return (<div>{this.props.children}</div>);
