c2b579701341056ff9a75724deed8eb34dc616e4
Durgaprasad Budhwani <durgaprasad.budhwani@gmail.com>
2017-12-08 13:00:34 +0530

    Added failure test case

--- __tests__/components/content/blog/index.spec.js
+++ __tests__/components/content/blog/index.spec.js
@@ -1,17 +1,29 @@
 /**
  * Created by Durgaprasad Budhwani on 10/9/2017.
  */
-/* global expect,it */
+/* global expect,it, describe, afterEach */
 import React from 'react';
 import BlogContainer from '../../../../src/components/content/blog';
 import renderer from 'react-test-renderer';
+import {mount} from 'enzyme'
+import fetchMock from 'fetch-mock'
 
-it('renders blog container', () => {
-    const tree = renderer.create(
-        <BlogContainer
-            top={10}
-            height={120}
-        />
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+describe("Blog Render", () => {
+    afterEach(() => {
+        fetchMock.restore();
+    });
+    
+    it('renders blog container', () => {
+        let blogs = [{image: 'image', title: "title", day: "day", month: "month"}];
+        fetchMock.mock('blogs.json', blogs);
+        const tree = renderer.create(
+            <BlogContainer
+                top={10}
+                height={120}
+            />
+        );
+        let instance = tree.getInstance();
+        expect(tree.toJSON()).toMatchSnapshot();
+        expect.arrayContaining(instance.state);
+    });
 });
--- __tests__/services/index.spec.js
+++ __tests__/services/index.spec.js
@@ -1,15 +1,26 @@
 /**
  * Created by Durgaprasad Budhwani on 12/8/2017.
  */
-/* global expect,it */
+/* global expect,it, describe, afterEach */
 import {GetBlogs} from '../../src/services';
 import fetchMock from 'fetch-mock'
 
 describe("Services", () => {
+    afterEach(() => {
+        fetchMock.restore();
+    });
+    
     it('GetBlogs should return list of blogs', async () => {
         fetchMock.mock('blogs.json', [{id: 1}, {id:2}]);
         let blogs = await GetBlogs();
         expect.arrayContaining(blogs);
         expect(blogs).toHaveLength(2);
+    });
+    
+    it('GetBlogs should return empty list if services return error code', async () => {
+        fetchMock.mock('blogs.json', 301);
+        let blogs = await GetBlogs();
+        expect.arrayContaining(blogs);
+        expect(blogs).toHaveLength(0);
     });
 });
