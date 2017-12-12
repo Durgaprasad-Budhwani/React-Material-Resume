f5c19ffd9999a88623e8ed0eaaefb29172b8f524
Durgaprasad Budhwani <durgaprasad.budhwani@gmail.com>
2017-12-12 14:13:13 +0530

    Added services test cases and add docs

--- __tests__/services/index.spec.js
+++ __tests__/services/index.spec.js
@@ -2,7 +2,7 @@
  * Created by Durgaprasad Budhwani on 12/8/2017.
  */
 /* global expect,it, describe, afterEach */
-import {GetBlogs} from '../../src/services';
+import {GetBlogs, GetExperience, GetEducation} from '../../src/services';
 import fetchMock from 'fetch-mock'
 
 describe("Services", () => {
@@ -22,5 +22,33 @@ describe("Services", () => {
         let blogs = await GetBlogs();
         expect.arrayContaining(blogs);
         expect(blogs).toHaveLength(0);
+    });
+    
+    it('GetExperience should return list of experience', async () => {
+        fetchMock.mock('experience.json', [{id: 1}, {id:2}]);
+        let experiences = await GetExperience();
+        expect.arrayContaining(experiences);
+        expect(experiences).toHaveLength(2);
+    });
+    
+    it('GetExperience should return empty list if services return error code', async () => {
+        fetchMock.mock('experience.json', 301);
+        let experiences = await GetExperience();
+        expect.arrayContaining(experiences);
+        expect(experiences).toHaveLength(0);
+    });
+    
+    it('GetEducation should return list of education', async () => {
+        fetchMock.mock('education.json', [{id: 1}, {id:2}]);
+        let educations = await GetEducation();
+        expect.arrayContaining(educations);
+        expect(educations).toHaveLength(2);
+    });
+    
+    it('GetEducation should return empty list if services return error code', async () => {
+        fetchMock.mock('education.json', 301);
+        let educations = await GetEducation();
+        expect.arrayContaining(educations);
+        expect(educations).toHaveLength(0);
     });
 });
