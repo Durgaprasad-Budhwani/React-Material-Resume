20922bb0692f029789260a12699fe79d2785fa1d
Durgaprasad-Budhwani <durgaprasad.budhwani@gmail.com>
2018-10-28 10:43:34 +0000

    Updates

---
+++ precache-manifest.879bd1fab476cc757bd2d941f772f274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3920d585e9865a44b37c",
+    "url": "/static/js/main.3920d585.chunk.js"
+  },
+  {
+    "revision": "b0b0d80e8650d2b6f3f1",
+    "url": "/static/js/1.b0b0d80e.chunk.js"
+  },
+  {
+    "revision": "3920d585e9865a44b37c",
+    "url": "/static/css/main.da23211c.chunk.css"
+  },
+  {
+    "revision": "b0b0d80e8650d2b6f3f1",
+    "url": "/static/css/1.f1ee7021.chunk.css"
+  },
+  {
+    "revision": "7c13a89bbd32fcdcc58991004ab6e93f",
+    "url": "/index.html"
+  }
+];
